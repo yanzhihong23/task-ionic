f5f6005e201668a3016ef56e2406cca152973674
严志宏 <yanzhihong23@qq.com>
2015-11-20 00:24:23 +0800

    fix headers and add mock data

--- src/app/services/app.service.js
+++ src/app/services/app.service.js
@@ -24,12 +24,16 @@
 					zjtoken = search.zjtoken || $search.zjtoken,
       		storeId = search.store_id || $search.store_id;
 
+  		// mock
+  		zjtoken = 'AA6C935B-519C-423D-A4CF-5E6E248D036B-15240-000006C5D1E6EBC6`7APVlAiIhUc8Vh02Wa0V5slLKb60If/N`52';
+  		storeId = 58;
+
   		if(!zjtoken || !storeId) {
   			utils.alert({
   				content: '请重新登录'
   			});
   		} else {
-  			headers = {
+  			var headers = {
   				'Content-Type': 'application/x-www-form-urlencoded',
   				'Cache-Control': 'max-age=60',
   				'zjtoken': zjtoken,
