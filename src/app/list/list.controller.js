08789fc25202f25f6ad5ebeefa06afdda9120c0d
Hom Yan <yanzhihong23@qq.com>
2015-11-20 15:24:03 +0800

    adjust list page layout

--- src/app/list/list.controller.js
+++ src/app/list/list.controller.js
@@ -6,7 +6,21 @@
     .controller('ListController', ListController);
 
   /** @ngInject */
-  function ListController() {
-    
+  function ListController($scope, $log, ApiService) {
+    $scope.onTabSelect = function(type) {
+      $log.debug(type);
+      switch(type) {
+        case 0: // all
+          break;
+        case 1: // published
+          break;
+        case 2: // accepted
+          break;
+        case 3: // completed
+          break;
+        case 4: // reviewed
+          break;
+      }
+    };
   }
 })();
