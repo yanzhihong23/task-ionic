790f1318d16a1348f18eff057c48e66f2fbe6dc4
Hom Yan <yanzhihong23@qq.com>
2015-11-30 09:37:09 +0800

    host url

--- src/app/index.constants.js
+++ src/app/index.constants.js
@@ -6,11 +6,12 @@
     .module('taskIonic')
     .constant('moment', moment)
     // .constant('HOST', 'http://b2b.zaijiadd.com')
-    .constant('HOST', 'http://192.168.16.158:8080')
+    .constant('HOST', 'http://192.168.16.75:8080')
     .factory('APISERVER', function($location, HOST) {
     	var host = /nonobank.com/.test($location.host()) ? $location.protocol() + '://' + $location.host() + ($location.port() ? ':' + $location.port() : '') : HOST;
 
-    	return host + '/zjdd-api';
+        // return host + '/zjdd-api';
+    	return host + '/zaijiadd-app';
     })
     .constant('$ionicLoadingConfig', {
 	    template: '<ion-spinner icon="bubbles" class="spinner-balanced"></ion-spinner>'
